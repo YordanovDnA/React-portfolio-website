45bf5e93bd558b38bfbd0c4d6300da8242ff143d
Yordanov <iliyanyyordanov@gmail.com>
2020-10-16 15:25:26 +0100

    About me page created, responsiveand ready
    Signed-off-by: Yordanov <iliyanyyordanov@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Switch, Route } from "react-router-dom";
 import HamburgerMenuIcon from './components/hamburgerMenuIcon';
 import HamburgerMenu from "./components/hamburgerMenu";
@@ -6,7 +6,7 @@ import NavBar from './components/navBar';
 import ProfileSection from './components/profileSecion';
 import Page404 from './components/404page';
 import HomePage from './components/homePage';
-import { useState } from 'react';
+import AboutMe from './components/aboutMe';
 
 function App() {
   const [open, setOpen] = useState(false);
@@ -23,12 +23,17 @@ function App() {
 
   return (
     <div className="row">
+
       <NavBar/>
+
       <ProfileSection/>
+
       <div className="main col">
+
         <div style={style} className="hamburgerMenuWrapper">
           <HamburgerMenu setOpen={setOpen} />
         </div>
+
         <div className="hamburgerMenuIcon">
           <HamburgerMenuIcon
               isOpen={open}
@@ -42,10 +47,13 @@ function App() {
               animationDuration={0.5}
             />
         </div>
+
         <Switch>
+          <Route path="/aboutMe" component={AboutMe} />
           <Route path="/" exact component={HomePage}/>
           <Route path="*" component={Page404} />
         </Switch>
+
       </div>
     </div>
   );
